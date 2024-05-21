7ecb7dad5b1a547cf8dc0ef53072a102163fdd52
tomkoevoets <tomkoevoets1@gmail.com>
2024-05-21 12:20:42 +0200

    Update bp4-opdracht 1

--- bp4-opdracht1/bp4-opdracht1.ino
+++ bp4-opdracht1/bp4-opdracht1.ino
@@ -1,9 +1,18 @@
 void setup() {
-  // put your setup code here, to run once:
-
+  // set up the outputs
+  pinMode(13, OUTPUT);
+  pinMode(10, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // turn on port 13, off port 10
+  digitalWrite(13, HIGH);
+  digitalWrite(10, LOW);
+  // wait 1,5 seconds
+  delay(1500); 
+  // turn on port 10, off 13
+  digitalWrite(13, LOW); 
+  digitalWrite(10, HIGH);
+  // wait 1,5 seconds
+  delay(1500); 
 }
