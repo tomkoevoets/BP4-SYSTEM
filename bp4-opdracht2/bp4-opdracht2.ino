6b7c9190e2a9cd4fb205a333869c45c4398bf4ea
tomkoevoets <tomkoevoets1@gmail.com>
2024-05-21 15:58:25 +0200

    Update bp4-opdracht 2
    - opdracht af

--- bp4-opdracht2/bp4-opdracht2.ino
+++ bp4-opdracht2/bp4-opdracht2.ino
@@ -1,9 +1,38 @@
-void setup() {
-  // put your setup code here, to run once:
+// globale waardes
+//  een lijst met pinnen waarop de LEDs zijn aangesloten
+const int ledPins[] = {13, 12, 11, 10, 7, 6, 3, 2};
+/* Bereken het aantal LEDs door het aantal bytes van alle elementen te delen door het aantal bytes van 1 element 
+ sizeof berekend dus in bytes waardoor het werkt om alle elementen te delen door 1 (op positie 0 in de array) 
+ https://www.w3schools.com/cpp/cpp_arrays_size.asp
+ */
+const int numLeds = sizeof(ledPins) / sizeof(ledPins[0]);  
+ // Wacht 75 milliseconden tussen elke actie
+const int delayTime = 75; 
 
+void setup() {
+  // Zet alle pinnen in de lijst ledPins in de OUTPUT stand
+  for (int i = 0; i < numLeds; i++) {
+    pinMode(ledPins[i], OUTPUT);  
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Zet elke LED één voor één aan en weer uit
+  for (int i = 0; i < numLeds; i++) {
+    // Zet de LED aan
+    digitalWrite(ledPins[i], HIGH);  
+    delay(delayTime);
+    // Zet de LED uit
+    digitalWrite(ledPins[i], LOW);  
+  }
 
+  // Zet elke LED één voor één aan en uit in omgekeerde volgorde
+  for (int i = numLeds - 1; i >= 0; i--) {
+    // Zet de LED aan
+    digitalWrite(ledPins[i], HIGH);  
+    delay(delayTime);  
+    // Zet de LED uit
+    digitalWrite(ledPins[i], LOW);  
+  }
 }
+
